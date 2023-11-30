3356b82bb4031870fdaea33337b6a03beddbea4e
Varna-Gobbur <152452063+Varna-Gobbur@users.noreply.github.com>
2023-11-29 17:05:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
+// server.js
 const express = require('express');
-const logger = require('morgan');
 const path = require('path');
-const server = express();
+const bodyParser = require('body-parser');
 
-server.use(express.urlencoded({ extended: true }));
-server.use(logger('dev'));
+const app = express();
+const port = process.env.PORT || 3000;
 
-// Serve static files from the project root
-const publicServedFilesPath = path.join(__dirname);
-server.use(express.static(publicServedFilesPath));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Serve the HTML file
-server.get('/', (req, res) => {
-  res.sendFile(path.join(publicServedFilesPath, 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Handle form submission
-server.post('/', (req, res) => {
-  const { noun, verb, adjective, adverb, place } = req.body;
-  const madLib = `Once upon a time, a ${adjective} ${noun} wanted to ${verb} ${adverb} in ${place}. The end.`;
+// Handle POST requests to '/'
+app.post('/', (req, res) => {
+  // Access form data from req.body
+  const { color1, color2, adjective1, pluralNoun, place } = req.body;
+
+  // Your mad lib logic here
+  const madLib = `Once upon a time in a land of ${color1} and ${color2}, there lived a ${adjective1} group of ${pluralNoun}. One day, they decided to embark on a journey to the enchanting ${place}. The landscape was painted with hues of ${color1} and ${color2}, creating a magical and ${adjective1} atmosphere. It was a truly ${adjective1} adventure!`;
+
+  // Send the filled-in mad lib as the response
   res.send(madLib);
 });
 
@@ -29,6 +34,6 @@ if (process.argv[2] === 'local') {
 }
 
 // Allow the server to accept connections from any IP address
-server.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Hey ! Server is working on port ${port}`);
 });
